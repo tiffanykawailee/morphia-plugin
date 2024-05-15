6c1abf538c0069777bef46ebb87b1716bd6d7642
Tiffany Lee <tiffany.lee@mongodb.com>
2024-05-15 11:17:49 -0400

    element util

--- src/main/java/com/mongo/morphiadeprecationplugin/util/PsiElementUtil.java
+++ src/main/java/com/mongo/morphiadeprecationplugin/util/PsiElementUtil.java
@@ -1,12 +1,9 @@
 package com.mongo.morphiadeprecationplugin.util;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiImportStatement;
 import com.intellij.psi.PsiJavaCodeReferenceElement;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiReferenceFactory;
 
 public class PsiElementUtil {
 
@@ -17,8 +14,11 @@ public class PsiElementUtil {
   }
 
   public PsiImportStatement createBaseDaoImportStatement() {
-    PsiClass aClass = psiElementFactory.createClassFromText("BaseDao").;
-    return psiElementFactory.createImportStatement(aClass);
+    PsiJavaCodeReferenceElement packageReferenceElement = psiElementFactory.createPackageReferenceElement(
+        "com.xgen.cloud.common.dao.base._public.impl");
+
+    return psiElementFactory.createImportStatement(
+        psiElementFactory.createClassFromText("BaseDao", packageReferenceElement.getReferenceNameElement()));
   }
 
   public PsiJavaCodeReferenceElement createBaseDaoReference() {
